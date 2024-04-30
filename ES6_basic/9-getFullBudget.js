c2a94d3cee7d35c59b0a7ed825f1a0eb57bfa525
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-04-30 13:35:17 -0500

    rm returns

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -4,12 +4,8 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: function (income) {
-      return `$${income}`;
-    },
-    getIncomeInEuros: function (income) {
-      return `${income} euros`;
-    },
+    getIncomeInDollars: function (income),
+    getIncomeInEuros: function (income),
   };
 
   return fullBudget;

4117794965793f9bc77cfe443a278fa1dfe5b5b5
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-04-30 13:49:37 -0500

    return new array with map method

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,3 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
-    var value = array[idx];
-    array[idx] = appendString + value;
-  }
-
-  return array;
+  return array.map(value => appendString + value);
 }

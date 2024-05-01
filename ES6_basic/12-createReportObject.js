b4b885f3d225c18191985bfc4d41cb456afd8875
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-01 13:06:27 -0500

    count departments objecykeysfunclength

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,6 +1,13 @@
 export default function createReportObject(employeesList) {
   const allEmployees = {};
   for (const department in employeesList) {
-    allEmployees[department] = employeesList[department];
+    if (Object.prototype.hasOwnProperty.call(employeesList, department)) {
+      allEmployees[department] = employeesList[department];
+    }
   }
+  const getNumberOfDepartments = (employees) => Object.keys(employees).length;
+  return {
+    allEmployees,
+    getNumberOfDepartments,
+  };
 }

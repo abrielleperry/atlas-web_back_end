e7d0c1cb28c38a747b01b08a262c92887824eada
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-02 14:00:49 -0500

    add errors and change to value

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -17,6 +17,9 @@ class Pricing {
   }
 
   set amount(newAmount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a string');
+    }
     this._amount = newAmount;
   }
 
@@ -25,6 +28,9 @@ class Pricing {
   }
 
   set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
     this._currency = value;
   }
 

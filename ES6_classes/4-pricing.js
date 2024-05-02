f6424b973ee57279e570c248251a45e9f5658991
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-02 13:38:56 -0500

    getters and setters for amount and currency

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,1 +1,42 @@
 import Currency from "./3-currency";
+
+class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a string');
+    }
+    if (typeof currency !== 'currency') {
+      throw new TypeError('Currency must be a string');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._curreny;
+  }
+
+  set currency(newCurrency) {
+    if (typeof newCurrencyt !== 'string') {
+      throw new TypeError('Currency must be a string');
+    }
+    this._currency = newCurrency;
+  }
+
+  displayFullCurrency() {
+    return `${this._amount} (${this._currency})`;
+  }
+}
+
+export default Pricing;

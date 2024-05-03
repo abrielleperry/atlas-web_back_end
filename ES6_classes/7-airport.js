a027074919ac3aeaeb900b52e312978e627acfc2
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-03 11:34:39 -0500

    adding 7 main to test

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -3,4 +3,10 @@ class Airport {
     this._name = name; // 'this' automatically refers to the new object instance
     this._code = code;
   }
+
+  toString() {
+    return `${this._name} (${this._code})`;
+  }
 }
+
+export default Airport;

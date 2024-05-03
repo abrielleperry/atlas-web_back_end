696248ae09f0982ee3725dcd0f2b4a650d89c7f8
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-03 11:51:19 -0500

    return object and convert to string

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -3,5 +3,13 @@ class HolbertonClass {
     this._size = size;
     this._location = location;
   }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
 }
 export default HolbertonClass;

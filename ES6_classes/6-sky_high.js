0ae0b409a92f58d3d28507feb6f4f903d9b20853
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-03 11:09:42 -0500

    super param and under param

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,5 +1,8 @@
 import Building from './5-building.js';
 
 class SkyHighBuilding extends Building {
-  constructor(sqft, floors)
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 }

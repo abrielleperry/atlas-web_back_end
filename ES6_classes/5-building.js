58f50128b3c665986cff6846b45a55a8724a50fd
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-02 19:34:51 -0500

    this.

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -7,8 +7,10 @@ class Building {
     return this._sqft;
   }
 
-  static evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  evacuationWarningMessage() {
+    if (this) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 }
 

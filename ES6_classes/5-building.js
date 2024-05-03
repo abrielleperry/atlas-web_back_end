ef503602e9cf4d499d3c2d2c6e1df28bb8e006cc
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-02 19:25:07 -0500

    static method error

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,5 +2,14 @@ class Building {
   constructor(sqft) {
     this._sqft = sqft;
   }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  static evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
 }
+
 export default Building;

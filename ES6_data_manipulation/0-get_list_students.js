e1f92ea59823d9d3315df26425aadacc4cc09fbc
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-06 13:06:48 -0500

    adding personal notes

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,10 @@
 function getListStudents() {
-  const students = [
+  const students = [ // define array of student objects
     { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstname: 'James', location: 'Columbia' },
     { id: 5, firstname: 'Serena', location: 'San Francisco' },
   ];
-  return students;
+  return students; // return array of student objects
 }
 
 export default getListStudents;

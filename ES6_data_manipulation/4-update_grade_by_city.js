8bf31ec5a9ebed553710530aafa1d1658d05de7f
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-06 14:27:23 -0500

    filter students by city

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,5 @@
 function updateStudentGradeByCity(students, city, newGrades) {
-
+  const studentsInCity = students.filter(student => student.city === city);
 }
 
 export default updateStudentGradeByCity;

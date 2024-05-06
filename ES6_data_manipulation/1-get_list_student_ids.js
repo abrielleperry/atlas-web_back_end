4938f4e20f58a71eba1f367b0f9c1f56090a08cd
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-06 13:15:40 -0500

    check if students in an array

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,8 @@
-function getListStudentIds {
-
+function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.map((student) => student.id);
 }
 
 export default getListStudentIds;

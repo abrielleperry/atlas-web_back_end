c405a523fdac96dce54e74617cedbf56a352566a
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-07 12:24:54 -0500

    result string and foreach iterate

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 function cleanSet(set, startString) {
+  let result = ''; // initialize empty string to store final out put
+  set.forEach(value => { // foreach to iterate over each value in the set
 
+  })
 }
 export default cleanSet;

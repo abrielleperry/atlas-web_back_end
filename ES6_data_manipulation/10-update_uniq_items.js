7180cfc6f81ef447c9164e1268f359a7fd88ab8c
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-07 13:43:23 -0500

    check if map

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,12 @@
-function updateUniqueItems {
-
+function updateUniqueItems(items) {
+  if (!(items instanceof Map)) {
+    throw Error('Cannot process');
+  }
+  items.forEach((quantity, item) => {
+    if (quantity === 1) {
+      items.set(item, 100);
+    }
+  });
+  return items;
 }
 export default updateUniqueItems;

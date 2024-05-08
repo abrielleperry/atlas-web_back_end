f59e82b94c4d71d4da7afe3f61581651d75e772c
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-08 11:59:44 -0500

    add personal notes

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,20 @@
+// func takes promise as arg
 function handleResponseFromAPI(promise) {
+  // return new promise obj
   return new Promise((resolve, reject) => {
+    // arrach then method to promise passed as arg
     promise
       .then(() => {
         console.log('Got a response from the API');
+        // resolve promise with obj containing status and body
         resolve({
           status: 200,
           body: 'success',
         });
       })
+      // attach catch method to handle errors during promise execution
       .catch(() => {
+        // reject promise with new Error obj
         reject(new Error());
       });
   });

66c2832ad28ced3b02f82d8a1dcddd888b3524d8
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-08 11:51:02 -0500

    resolve and reject

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,4 +1,13 @@
 function handleResponseFromAPI(promise) {
-  return new Promise
+  return new Promise() => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject new Error;
+    }
+  }
 }
 export default handleResponseFromAPI;

9bb39b7307a7f7356223e5e92d56530b868ecee3
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-08 13:22:32 -0500

    then and catch method

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,5 +1,17 @@
 import { uploadPhoto, createUser } from './utils';
-function handleProfileSignup() {
 
+function handleProfileSignup() {
+  const photo = 'photo-profile-1';
+  const user = { firstName: 'Guillaume', lastName: 'Salva' };
+  Promise.all([
+    uploadPhoto(photo),
+    createUser(user),
+  ])
+    .then(([photoResult, userResult]) => {
+      console.log(photoResult, userResult.firstName, userResult.lastName);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
 export default handleProfileSignup;
---
+++ ES6_promise/utils.js
@@ -0,0 +1,13 @@
+export function uploadPhoto() {
+  return {
+    status: 200,
+    body: 'photo-profile-1',
+  };
+}
+
+export function createUser() {
+  return {
+    firstName: 'Guillaume',
+    lastName: 'Salva',
+  };
+}

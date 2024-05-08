0eb0f3cb42ea06093930d70e0296172fe1110415
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-08 15:35:45 -0500

    handle rejected promise from uploadphoto

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,10 +4,11 @@ import uploadPhoto from './5-photo-reject';
 function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPromise = uploadPhoto(fileName);
-  return Promise.all([signUpPromise, uploadPromise]).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  })));
+  return Promise.allSettled([signUpPromise, uploadPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
 
 export default handleProfileSignup;

7e0bf983fa9c5307301525ff17c5bb9f50994012
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-08 16:06:34 -0500

    correct 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  Promise.all([
-    uploadPhoto(),
-    createUser(),
-  ])
+  Promise.all([uploadPhoto(), createUser()])
     .then(([photoResult, userResult]) => {
       console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
     })

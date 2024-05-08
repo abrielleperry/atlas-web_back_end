9783fd70818d1ea2078fbc0dd362facf75b26182
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-08 13:29:30 -0500

    photoresult

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,15 +3,17 @@ import { uploadPhoto, createUser } from './utils';
 function handleProfileSignup() {
   const photo = 'photo-profile-1';
   const user = { firstName: 'Guillaume', lastName: 'Salva' };
+
   Promise.all([
     uploadPhoto(photo),
     createUser(user),
   ])
     .then(([photoResult, userResult]) => {
-      console.log(`${photoResult} ${userResult.firstName} ${userResult.lastName}`);
+      console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
     });
 }
+
 export default handleProfileSignup;

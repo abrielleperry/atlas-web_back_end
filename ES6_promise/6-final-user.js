383587785714a48aee705e9e042f9b13836dc1c7
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-05-08 14:16:47 -0500

    consts

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,4 +2,6 @@ import { signUpUser } from './4-user-promise';
 import { uploadPhoto } from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
 }

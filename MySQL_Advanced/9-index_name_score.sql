f1591c85a933179a4947a1e78c1b81997956af45
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-01 20:02:17 -0500

    fix

--- MySQL_Advanced/9-index_name_score.sql
+++ MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,3 @@
 -- create the index using the first letter and score
-ALTER TABLE names ADD COLUMN first_letter CHAR(1) GENERATED ALWAYS AS (SUBSTRING(name, 1, 1)) STORED;
-
-CREATE INDEX idx_name_first_score ON names (first_letter, score);
+-- Create an index on the first letter of the name and the score
+CREATE INDEX idx_name_first_score ON names ((SUBSTRING(name, 1, 1)), score);

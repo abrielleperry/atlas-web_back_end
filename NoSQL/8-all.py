8d2d0e3f7771263efe2ea4d554e79ac6ea0d6d68
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-07 12:45:33 -0500

    if mongo coll return if not empty list

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
 
+
 def list_all(mongo_collection):
     """function that lists all documents in a collection"""
+    return list(mongo_collection.find()) if mongo_collection else []

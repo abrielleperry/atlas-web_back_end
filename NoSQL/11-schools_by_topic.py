8788985d2d4f6d733c30ff484331f37c4be1a069
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-07 13:16:46 -0500

    typo in

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""function that returns the list of school having a specific topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns the list of school having a specific topic"""
+    return [school for school in mongo_collection.find()
+            if topic in school['topic']]

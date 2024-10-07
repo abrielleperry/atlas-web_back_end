37d2a9f3afa2a95c185505b9a9a8994c30171db0
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-07 13:13:06 -0500

    changes made

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -3,4 +3,7 @@
 
 def update_topics(mongo_collection, name, topics):
     """func that changes all topics of a school document based on the name"""
-    
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )

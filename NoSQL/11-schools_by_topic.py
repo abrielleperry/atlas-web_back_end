3cd5b3eb146f1f34a6d710fcda7aed313d9b4713
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-07 13:18:50 -0500

    return list

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,5 +4,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """returns the list of school having a specific topic"""
-    return [school for school in mongo_collection.find()
-            if topic in school['topic']]
+    return list(mongo_collection.find({'topics': {'$in': [topic]}}))

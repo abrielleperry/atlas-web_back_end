1841f34e17626b095a6f1af63f12934792ffb108
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-07 12:58:59 -0500

    add 9 and 9 main

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-
+"""function that lists all documents in a collection"""
 
 
 def list_all(mongo_collection):
-    """function that lists all documents in a collection"""
-    if mongo_collection is None:
-        return []
+    """Return an empty list if no document in the collection"""
     return list(mongo_collection.find())

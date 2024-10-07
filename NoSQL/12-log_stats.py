2ff2dbc75b0ef0f310ea0369d07a4dd761d3c2c7
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-07 13:30:28 -0500

    log

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+import pymango
+
+
+def log_stats():
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
+    total_logs = collection.count_documents({})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+    status_check_count = collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+
+if __name__ == "__main__":
+    log_stats()

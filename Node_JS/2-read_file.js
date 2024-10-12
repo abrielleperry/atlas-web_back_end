a9d2bbf2ffd9a74223bbf182113802982a3ef559
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-12 11:29:14 -0500

    import and try consts

--- Node_JS/2-read_file.js
+++ Node_JS/2-read_file.js
@@ -1,4 +1,11 @@
-function countStudents(/)
+const fs = require('fs');
+
+function countStudents(path) {
+    try {
+        const data = fs.readFileSync(path, 'utf-8')
+        const lines = data.split('\n').filter(line => line.trim() !== '');
+    }
 Cannot load the database
 Number of students: NUMBER_OF_STUDENTS
 Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES
+}

9f12cdb12ef00ea9a2658eae8c8517c07c5ce1c5
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-12 11:30:10 -0500

    throw error

--- Node_JS/2-read_file.js
+++ Node_JS/2-read_file.js
@@ -4,8 +4,10 @@ function countStudents(path) {
     try {
         const data = fs.readFileSync(path, 'utf-8')
         const lines = data.split('\n').filter(line => line.trim() !== '');
+        if (lines.length === 0) {
+            throw new Error('Cannot load the database')
+        }
     }
-Cannot load the database
 Number of students: NUMBER_OF_STUDENTS
 Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES
 }

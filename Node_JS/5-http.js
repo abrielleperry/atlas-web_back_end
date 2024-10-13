1a961dd670c2d6f1572cb65bf94e041bc7398b79
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-13 12:39:48 -0500

    cp 3 to 5

--- Node_JS/5-http.js
+++ Node_JS/5-http.js
@@ -11,9 +11,36 @@ const app = http.createServer((req, res) => {
   } else if (req.url === '/students') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
+    fs.readFile(databaseFile, 'utf-8', (err, data) => {
+      if (err) {
+        res.write('Cannot load the database');
+        res.end();
+        return;
+      }
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1).map((line) => line.split(','));
+
+      res.write(`Number of students: ${students.length}\n`);
+      const fields = lines[0].split(',');
+      const studentGroups = students.reduce((acc, student) => {
+        const studentData = student.split(',');
+        const field = studentData[fields.indexOf('field')];
+        if (!acc[field]) {
+          acc[field] = [];
+        }
+        acc[field].push(studentData[fields.indexOf('firstname')]);
+        return acc;
+      }, {});
+      for (const [field, names] of Object.entries(studentGroups)) {
+        res.write(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`);
+      }
+      res.end();
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('Not found');
     res.end();
   }
-
 });
 
 app.listen(1245);

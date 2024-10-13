b9674aab4e102c38aa0d6d891265775d6db1529d
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-13 11:40:21 -0500

    return promise and error handling

---
+++ Node_JS/3-read_file_async.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+    });
+  });
+}
+
+module.exports = countStudents;

90332e4734a3c7bfd0fd9987ae694a453570b566
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-13 12:28:11 -0500

    students path

---
+++ Node_JS/5-http.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const databaseFile = process.argv[2];
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello Holberton School!');
+    res.end();
+  } else if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
+    res.end();
+  }
+
+});
+
+app.listen(1245);
+module.exports = app;

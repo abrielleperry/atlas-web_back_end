8ea5eec4aee891004267170d4fa042715e7b7afb
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-14 11:13:37 -0500

    list of students but not grouped

--- Node_JS/7-http_express.js
+++ Node_JS/7-http_express.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const fs = require('fs').promises;
+const { join } = require('path');
 
 const app = express();
+
+app.listen(1245);
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.use((req, res) => {
-  const { path } = req;
-  res.status(404).send(`Cannot GET ${path}`);
+app.get('/students', async (req, res) => {
+  const db = req.query.db || 'database.csv';
+  try {
+    const students = await fs.readFile(join(__dirname, db), 'utf8');
+    const trimmedStudents = students.trimEnd().split('\n').filter(Boolean);
+    res.send(`This is the list of out students:\n\n${trimmedStudents.join('\n')}`);
+  } catch (error) {
+    console.error('Error reading file:', error);
+    res.status(500).send('An error occured while processing the request.');
+  }
 });
-
-app.listen(1245);
 
 module.exports = app;

0a506ef5dbe0a2cd4a781ec39fdf738f108f62c1
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-18 16:09:35 -0500

    define jobs

--- queuing_system_in_js/7-job_processor.js
+++ queuing_system_in_js/7-job_processor.js
@@ -41,3 +41,22 @@ queue.on('job failed', (id, result) => {
     console.log(`Notification job ${job.id} failed: ${result.message}`)
   })
 })
+
+const notifications = [
+    { phoneNumber: '4153518743', message: 'This is the code 4321 to verify your account' },
+    { phoneNumber: '4153538781', message: 'This is the code 4562 to verify your account' },
+    { phoneNumber: '4153118782', message: 'This is the code 4321 to verify your account' },
+    { phoneNumber: '4153718781', message: 'This is the code 4562 to verify your account' },
+    { phoneNumber: '4159518782', message: 'This is the code 4321 to verify your account' },
+    { phoneNumber: '4158718781', message: 'This is the code 4562 to verify your account' },
+    { phoneNumber: '4153818782', message: 'This is the code 4321 to verify your account' },
+    { phoneNumber: '4154318781', message: 'This is the code 4562 to verify your account' },
+    { phoneNumber: '4151218782', message: 'This is the code 4321 to verify your account' },
+];
+
+notifications.forEach(({ phoneNumber, message }) => {
+  const job = queue.create('push_notification_code_2', { phoneNumber, message })
+    .save((err) => {
+      if (!err) console.log(`Enqueued job with phone number: ${phoneNumber}`);
+    });
+});

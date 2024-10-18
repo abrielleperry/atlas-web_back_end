dedd9b4ed1dcef3f3a690addf294ec15f48f7301
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-18 12:52:29 -0500

    process jobs from queue

--- queuing_system_in_js/6-job_processor.js
+++ queuing_system_in_js/6-job_processor.js
@@ -5,3 +5,9 @@ const queue = kue.createQueue();
 function sendNotification(phoneNumber, message) {
   console.log(`Sending notifiction to ${phoneNumber}, with message: ${message}`);
 }
+
+queue.process('push_notification_code', (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message);
+  done();
+});

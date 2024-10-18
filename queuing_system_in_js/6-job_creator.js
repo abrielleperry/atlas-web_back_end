17372e5a2507b75fb9f64d288e6b6f4ade285a36
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-18 13:11:48 -0500

    notifcation sent

--- queuing_system_in_js/6-job_creator.js
+++ queuing_system_in_js/6-job_creator.js
@@ -1,19 +1,24 @@
-const kue = require('kue');
+import kue from 'kue';
 
 const queue = kue.createQueue();
 
 const jobData = {
-  phoneNumber: '1234567890',
-  message: 'This is a test message'
+    phoneNumber: '4153518780',
+    message: 'This is the code to verify your account',
 };
 
-const job = queue.create('push_notifcations_code', jobData)
-  .save((err) => {
-    if (!err) console.log(`Notification job created: ${job.id}`);
-  });
+const job = queue.create('push_notification_code', jobData);
+
+job.on('enqueue', () => {
+    console.log(`Notification job created: ${job.id}`);
+});
 
 job.on('complete', () => {
-  console.log('Notification job completed')
-}).on('failed', () => {
-  console.log('Notification job failed')
+    console.log('Notification job completed');
 });
+
+job.on('failed', () => {
+    console.log('Notification job failed');
+});
+
+job.save();

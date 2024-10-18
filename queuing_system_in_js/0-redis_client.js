1f722b7de0fd0354676688ffa7af881eb6ae3491
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-18 10:39:28 -0500

    error message

--- queuing_system_in_js/0-redis_client.js
+++ queuing_system_in_js/0-redis_client.js
@@ -6,5 +6,7 @@ client.on('connect', () => {
   console.log('Redis client connected to the server');
 });
 
-
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
 

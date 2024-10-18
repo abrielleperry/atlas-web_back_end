1b16af6e6377c877044be9f2b67f3645dbff5545
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-18 12:43:50 -0500

    create job in push queue

--- queuing_system_in_js/6-job_creator.js
+++ queuing_system_in_js/6-job_creator.js
@@ -6,3 +6,8 @@ const jobData = {
   phoneNumber: '1234567890',
   message: 'This is a test message'
 };
+
+const job = queue.create('push_notifcations_code', jobData)
+  .save((err) => {
+    if (!err) console.log(`Notifcation job created: ${jov.id}`);
+  });

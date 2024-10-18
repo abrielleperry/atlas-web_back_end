477ed1d2f32cc5d460616fb04de91c5c9de34c43
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-18 16:00:28 -0500

    log job process

--- queuing_system_in_js/7-job_processor.js
+++ queuing_system_in_js/7-job_processor.js
@@ -1,6 +1,5 @@
 import kue from 'kue';
 
-const queue = kue.createQueue();
 
 const blacklistedNumbers = ['4153518780', '4153518781'];
 
@@ -13,9 +12,32 @@ const sendNotification = (phoneNumber, message, job, done) => {
 
   job.progress(50, 100)
   console.log(`Sending notification to ${phoneNumber}, with message: ${message}`)
+  done();
 }
 
-queue.process('push_notification_code', 2, (job, done) => {
+const queue = kue.createQueue();
+
+queue.process('push_notification_code_2', 2, (job, done) => {
   const { phoneNumber, message } = job.data;
   sendNotification(phoneNumber, message, job, done)
+})
+
+queue.on('job enqueue', (id) => {
+  kue.Job.get(id, (err, job) => {
+    if (err) return;
+    console.log(`Notification job ${job.id} 0% complete`)
+  })
+})
+
+queue.on('job.complete', (id) => {
+  kue.Job.get(id, (err, job) => {
+    if (err) return;
+    console.log(`Notification job ${job.id} completed`)
+  })
+})
+
+queue.on('job failed', (id, result) => {
+  kue.Job.get(id, (err, job) => {
+    console.log(`Notification job ${job.id} failed: ${result.message}`)
+  })
 })

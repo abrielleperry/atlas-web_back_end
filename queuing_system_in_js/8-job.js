21e8a5aa19df8d009a57e07270b46b946cc8b00a
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-21 11:10:05 -0500

    check of jobs is an array if not throw error

---
+++ queuing_system_in_js/8-job.js
@@ -0,0 +1,17 @@
+const kue = require('kue');
+
+function createPushNotificationsJobs(jobs, queue) {
+    if (!Array.isArray(jobs)) {
+        throw new Error('Jobs is not an array');
+    }
+
+    jobs.forEach((jobData) => {
+        const job = queue.create('push_notification_code_3', jobData)
+            .save((err) => {
+                if (!err) {
+                    console.log(`Notification job created: ${job.id}`);
+                }
+            });
+
+
+module.exports = createPushNotificationsJobs;

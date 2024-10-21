eda07d8d36a366b9afcfad44c4d3c2750b2af72b
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-21 10:56:20 -0500

    clear queue exit test mode

--- queuing_system_in_js/8-job.test.js
+++ queuing_system_in_js/8-job.test.js
@@ -1,3 +1,15 @@
 import { expect } from 'chai';
 import kue from 'kue';
 import createPushNotificationsJobs from './8-job.js';
+
+const queue = kue.createQueue();
+
+describe('createPushNotificationsJobs', () => {
+  beforeEach(() => {
+    queue.testMode.enter();
+  });
+  afterEach(() => {
+    queue.testMode.clear();
+    queue.testMode.exit();
+  });
+})

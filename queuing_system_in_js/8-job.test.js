a27e5ff15c4f35703ea055c3382a2c4408892121
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-21 11:34:44 -0500

    hook runs before each test

---
+++ queuing_system_in_js/8-job.test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import kue from 'kue';
+import createPushNotificationsJobs from './8-job.js';
+
+describe('createPushNotificationsJobs', () => {
+    let queue;
+
+    beforeEach(() => {
+        queue = kue.createQueue();
+        queue.testMode.enter();
+    });

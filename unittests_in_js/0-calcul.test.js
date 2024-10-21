36e093d6fc99c15409d62a8710428d464f2c19b3
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-21 12:48:58 -0500

    add more stests

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -14,5 +14,15 @@ describe('calculateNumber', function() {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
-  it('should return 6')
-})
+  it('should return 6 when inputs are 1.5 and 3.7', function() {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return 0 when inputs are 0.1 and 0.3', function() => {
+    assert.strictEqual(calculateNumber(0.1, 0.3), 0);
+  });
+
+  it('should return -2 when inputs are -1.4 and -0.5', function() => {
+    assert.strictEqual(calculateNumber(-1.4, -0.5), -2);
+  });
+});

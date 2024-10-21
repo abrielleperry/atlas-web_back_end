06d6436cec7140faf2e9d42ade0f66975f824f77
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-21 11:59:42 -0500

    local tests correct

--- queuing_system_in_js/9-stock.js
+++ queuing_system_in_js/9-stock.js
@@ -5,14 +5,14 @@ const { promisify } = require('util');
 const app = express();
 const port = 1245;
 
-const list_products = [
+const listProducts = [
   { itemId: 1, itemName: 'Suitcase 250', price: 50, initialAvailableQuantity: 4 },
   { itemId: 2, itemName: 'Suitcase 450', price: 100, initialAvailableQuantity: 10 },
   { itemId: 3, itemName: 'Suitcase 650', price: 350, initialAvailableQuantity: 2 },
   { itemId: 4, itemName: 'Suitcase 1050', price: 550, initialAvailableQuantity: 5 },
 ];
 
-const getItemById = (id) => list_products.find(product => product.itemId === id);
+const getItemById = (id) => listProducts.find(product => product.itemId === id);
 
 const client = redis.createClient();
 const getAsync = promisify(client.get).bind(client);
@@ -36,19 +36,21 @@ app.get('/list_products/:itemId', async (req, res) => {
   const product = getItemById(itemId);
 
   if (!product) {
-    return res.json({ status: 'Product not found'});
+    return res.json({ status: 'Product not found' });
   }
+
   const currentQuantity = await getCurrentReservedStockById(itemId) || product.initialAvailableQuantity;
   res.json({ ...product, currentQuantity });
 });
 
-app.get('/reserve_products/:itemId', async (req, res) => {
+app.get('/reserve_product/:itemId', async (req, res) => {
   const itemId = parseInt(req.params.itemId, 10);
   const product = getItemById(itemId);
 
   if (!product) {
-    return res.json({ status: 'Product not found'});
+    return res.json({ status: 'Product not found' });
   }
+
   const currentQuantity = await getCurrentReservedStockById(itemId) || product.initialAvailableQuantity;
 
   if (currentQuantity <= 0) {
@@ -57,4 +59,8 @@ app.get('/reserve_products/:itemId', async (req, res) => {
 
   await reserveStockById(itemId, currentQuantity - 1);
   res.json({ status: 'Reservation confirmed', itemId });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });

a93f29cc691a8f3dff62a1787fc3d5368a463c48
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-21 11:40:31 -0500

    add json

--- queuing_system_in_js/9-stock.js
+++ queuing_system_in_js/9-stock.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const redis = require('redis');
 const { promisify } = require('util');
+
+const app = express();
+const port = 1245;
+
+const list_products = [
+  { itemId: 1, itemName: 'Suitcase 250', price: 50, initialAvailableQuantity: 4 },
+  { itemId: 2, itemName: 'Suitcase 450', price: 100, initialAvailableQuantity: 10 },
+  { itemId: 3, itemName: 'Suitcase 650', price: 350, initialAvailableQuantity: 2 },
+  { itemId: 4, itemName: 'Suitcase 1050', price: 550, initialAvailableQuantity: 5 },
+];
+

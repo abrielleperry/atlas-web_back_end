03db77827a4be05139363fdee4c1d5e86ef60255
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-22 10:18:14 -0500

    assertion tests

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -1,2 +1,10 @@
 const assert = require('assert');
 const { calculate } = require('./1-calcul');
+
+describe('calculate function', () => {
+  it('should sum two numbers', () => {
+    assert.strictEqual(calculate(5, 3, 'sum'), 8);
+    assert.strictEqual(calculate(-5, 3, 'sum'), -2);
+    assert.strictEqual(calculate(5, -3, 'sum'), 2);
+  })
+})

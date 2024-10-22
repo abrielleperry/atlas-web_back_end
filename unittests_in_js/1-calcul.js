b0e4b9216255fc36ba0634d74fb6f1f94827960f
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-22 10:10:55 -0500

    case sum sub div

--- unittests_in_js/1-calcul.js
+++ unittests_in_js/1-calcul.js
@@ -1,6 +1,19 @@
 function calculate(a, b, type) {
   const roundedA = Math.round(a);
   const roundedB = Math.round(b);
+  switch (type.toUpperCase()) {
+    case 'SUM':
+      return roundedA + roundedB;
+    case 'SUBTRACT':
+      return roundedA = roundedB;
+    case 'DIVIDE':
+      if (roundedB === 0) {
+        return 'Error';
+      }
+      return roundedA / roundedB;
+    default:
+      throw new Error('Invalid operation');
+  }
 }
 
 module.experts = { calculate };

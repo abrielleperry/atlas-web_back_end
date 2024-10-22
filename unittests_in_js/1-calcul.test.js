041188977d277e5e8ef3b57b4963b3c1711525cf
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-22 10:23:37 -0500

    more aserrtions tests

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -6,5 +6,29 @@ describe('calculate function', () => {
     assert.strictEqual(calculate(5, 3, 'sum'), 8);
     assert.strictEqual(calculate(-5, 3, 'sum'), -2);
     assert.strictEqual(calculate(5, -3, 'sum'), 2);
-  })
+  });
+
+  it('should subtract two numbers', () => {
+    assert.strictEqual(calculate(5, 3, 'subtract'), 2);
+    assert.strictEqual(calculate(-5, 3, 'subtract'), -8);
+    assert.strictEqual(calculate(5, -3, 'subtract'), 8);
+  });
+
+  it('should divide two numbers', () => {
+    assert.strictEqual(calculate(6, 2, 'divide'), 3);
+    assert.strictEqual(calculate(-6, 2, 'divide'), -3);
+    assert.strictEqual(calculate(6, -2, 'divide'), -3);
+    assert.strictEqual(calculate(6, 0, 'divide'), 'Error');
+    assert.strictEqual(calculate(0, 2, 'divide'), 0);
+  });
+
+  it('should round numbers before calculation', () => {
+    assert.strictEqual(calculate(3.7, 2.9, 'sum'), 6);
+    assert.strictEqual(calculate(3.7, 2.9, 'subtract'), 0.8);
+    assert.strictEqual(calculate(3.7, 2.9, 'divide'), 1.27);
+  });
+
+  it('should handle invalid operations', () => {
+    assert.throws(calculate(5, 3, 'invalid'), { name: 'Error' });
+  });
 })

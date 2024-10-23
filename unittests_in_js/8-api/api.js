8c19fb7162661a7f36654644b90ecf8ad9eb32c8
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-23 11:02:49 -0500

    listen to port

--- unittests_in_js/8-api/api.js
+++ unittests_in_js/8-api/api.js
@@ -4,3 +4,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
+
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865')
+});

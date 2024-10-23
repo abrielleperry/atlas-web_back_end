7e823aed8b38491c8a5aa0f075e953273aadbda7
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-23 11:59:38 -0500

    payment methods 12

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
+app.get('/', (request, response) => {
+  response.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  if (!isNaN(id)) {
-    const paymentMethods = [
-      { method: 'Credit Card', status: 'Active' },
-      { mnethod: 'Paypal', status: 'Pending' }
-    ];
-    res.json({ paymentMethods });
-  } else {
-    res.status(404).json({ error: 'Cart not found'});
+app.get('/cart/:id', (request, response) => {
+  const { id } = request.params;
+  if (isNaN(id)) {
+    return response.status(404).send('Id must be a number');
   }
-})
+  return response.send(`Payment methods for cart ${id}`);
+});
 
 app.listen(7865, () => {
   console.log('API available on localhost port 7865')

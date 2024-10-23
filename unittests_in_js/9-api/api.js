b55bdc72d661d730411cebcf71ca5fe816c1c00a
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-10-23 11:41:16 -0500

    route to get cart id

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -5,6 +5,19 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
+app.get('/cart/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  if (!isNaN(id)) {
+    const paymentMethods = [
+      { method: 'Credit Card', status: 'Active' },
+      { mnethod: 'Paypal', status: 'Pending' }
+    ];
+    res.json({ paymentMethods });
+  } else {
+    res.status(404).json({ error: 'Cart not found'});
+  }
+})
+
 app.listen(7865, () => {
   console.log('API available on localhost port 7865')
 });
